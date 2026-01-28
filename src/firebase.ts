3b9b5a622cd2f097ec810926893af3d3d0750790
James Hooson <jmhooson@hotmail.com>
2026-01-29 11:27:50 +1300

    fix: move API keys to environment variables

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,20 +1,16 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
-import { getAnalytics } from 'firebase/analytics'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD_-MkWdrUa5UvXXA9JspndwD8reqwBNvY',
-  authDomain: 'bonsai-fdf2b.firebaseapp.com',
-  projectId: 'bonsai-fdf2b',
-  storageBucket: 'bonsai-fdf2b.firebasestorage.app',
-  messagingSenderId: '729369432047',
-  appId: '1:729369432047:web:7319e38a4a2ce3e2eeb01a',
-  measurementId: 'G-Q9X7SBL3GS',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 }
 
 const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app)
 export const db = getFirestore(app)
 export const storage = getStorage(app)
-export { analytics }
